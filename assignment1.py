5c33641420559719b444c508b3e263f0570e4745
karthikreddyarem <karthikavreddy@gmail.com>
2020-08-18 08:40:38 +0530

    mulbranch

--- assignment1.py
+++ assignment1.py
@@ -2,16 +2,17 @@ def addition(a,b):
 	return a+b
 
 def subtraction(a,b):
-	return a-b;
+	return a-b
 
-
+def mul(a,b):
+	return a*b
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
-
+	print(mul(a,b))
 
 '''
 clone from github
