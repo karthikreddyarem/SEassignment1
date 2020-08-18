80332ed2dcdd9449ed71b9bb0f9449a85d956a0d
karthikreddyarem <karthikavreddy@gmail.com>
2020-08-18 08:43:22 +0530

    divbranch

--- assignment1.py
+++ assignment1.py
@@ -7,13 +7,17 @@ def subtraction(a,b):
 def mul(a,b):
 	return a*b
 
+def div(a,b):
+	return a/b
+
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
 	print(mul(a,b))
-
+	print(div(a,b))
+	
 '''
 clone from github
 Create new branches for Multiplication and Division
