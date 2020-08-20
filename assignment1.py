cc059408f36b1b379ec63a4346b573816e6d5565
karthikreddyarem <karthikavreddy@gmail.com>
2020-08-20 12:34:03 +0530

    div

--- assignment1.py
+++ assignment1.py
@@ -4,16 +4,18 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b
 
+def div(a,b):
+	return a/b
+
 def mul(a,b):
 	return a*b
-
-
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(div(a,b))
 	print(mul(a,b))
 	
 '''
